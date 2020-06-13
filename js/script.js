a8a088cde6289c890f0ae16e60782310f1066daa
Apayah <gdewira@yahoo.com>
2020-06-13 09:51:17 +0700

    pretty

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
+var searchbox = document.getElementById("search");
+var searchbutton = document.getElementById("button-search");
 
-var searchbox = document.getElementById('search');
-var searchbutton = document.getElementById('button-search')
-
-window.onload = function() {
-    var input = searchbox.focus();
+window.onload = function () {
+  var input = searchbox.focus();
 };
 
-searchbox.onkeypress = function(e) {
-	if (e.keyCode === 13) {
-		search();
-	}
-}
+searchbox.onkeypress = function (e) {
+  if (e.keyCode === 13) {
+    search();
+  }
+};
 
-searchbutton.addEventListener('click', search, false);
+searchbutton.addEventListener("click", search, false);
 
 function search() {
-	var query = searchbox.value;
-	window.location = 'https://duckduckgo.com/?q=' + query;
+  var query = searchbox.value;
+  window.location = "https://duckduckgo.com/?q=" + query;
 }
